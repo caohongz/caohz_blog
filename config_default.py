888200913f88c148359afafbefd8b89efc2093ba
caohongz <18511283565@163.com>
2019-04-19 21:53:14 -0400

    commit config file

---
+++ config_default.py
@@ -0,0 +1,14 @@
+# config_default.py
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'caohz',
+        'password': 'ahooge123',
+        'database': 'caohz_blog'
+    },
+    'session': {
+        'secret': 'CaoHz'
+    }
+}
