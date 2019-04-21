e6a33156b10cb7c9ef78715954ff012c4015aaf6
caohongz <18511283565@163.com>
2019-04-21 12:38:16 -0400

    complete commit add api

--- www/handlers.py
+++ www/handlers.py
@@ -12,7 +12,7 @@ from coroweb import get, post
 from models import User, Comment, Blog, next_id
 
 @get('/')
-async def index(request):
+def index(request):
     summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     blogs = [
         Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
@@ -23,3 +23,10 @@ async def index(request):
         '__template__': 'blogs.html',
         'blogs': blogs
     }
+
+@get('/api/users')
+async def api_get_users(*, page='1'):
+    users = await User.findAll(orderBy='created_at desc')
+    for u in users:
+        u.passwd = '******'
+    return dict(users=users)
