57f5c81d1ced419c5db373736d6bfccfa3fe341b
caohongz <18511283565@163.com>
2019-04-21 22:00:23 -0400

    final completely

--- www/orm.py
+++ www/orm.py
@@ -225,3 +225,4 @@ class Model(dict, metaclass=ModelMetaclass):
         rows = await execute(self.__delete__, args)
         if rows != 1:
             logging.warn('failed to remove by primary key: affected rows: %s' % rows)
+
